3e67a11a085612338b8bf9b37202627426c815d1
David Cantoran <davidcantoran36@gmail.com>
2019-02-20 05:01:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber(currLine, newName) {
   currLine.push(`${newName}`);
   return `Welcome, ${newName}. You are number ${currLine.length} in line.`;
 }
+
+function nowServing(currLine) {
+  
+  if (currLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+}
