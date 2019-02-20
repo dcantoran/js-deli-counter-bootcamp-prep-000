6fdc20177a92367c5472e62e6a7d09484c5462af
David Cantoran <davidcantoran36@gmail.com>
2019-02-20 05:08:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(currLine) {
 
 function currentLine(line) {
   if (line.length === 0) {
-    return "The line is currently empty";
+    return "The line is currently empty.";
   }
   return line;
 }
